bc3a41905c795b4b83934d82c2605b0d0b864c00
Christophe Godart <51cgo@lilo.org>
2025-07-15 11:29:03 +0200

    Fix text in debug logs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='sunshine-trigger',
-    version='1.0',
+    version='1.0.1',
     author='Christophe Godart',
     author_email='51CGO@lilo.org',
     description='Tool for executing commands on sunrise and sunset',
--- sunshine_trigger/sunshine_trigger.py
+++ sunshine_trigger/sunshine_trigger.py
@@ -22,10 +22,10 @@ class SunshineTrigger(threading.Thread):
         self.logger.debug("Now : %s" % now)
 
         self.next_sunrise = self.calendar.riseutc(now.date()).replace(tzinfo=datetime.timezone.utc)
-        self.logger.debug("Next self.next_sunrise : %s" % self.next_sunrise)
+        self.logger.debug("Next sunrise : %s" % self.next_sunrise)
         
         self.next_sunset = self.calendar.setutc(now.date()).replace(tzinfo=datetime.timezone.utc)
-        self.logger.debug("Next self.next_sunset : %s" % self.next_sunset)
+        self.logger.debug("Next sunset : %s" % self.next_sunset)
 
         if now > self.next_sunrise and now < self.next_sunset:
             self.sunshine = True
@@ -33,7 +33,6 @@ class SunshineTrigger(threading.Thread):
         else:
             self.sunshine = False
             self.logger.debug("Night")
-
 
     def run(self):
 
@@ -49,7 +48,7 @@ class SunshineTrigger(threading.Thread):
                     self.logger.debug("Night has fallen")                    
                     self.sunshine = False
                     self.next_sunrise = self.calendar.riseutc(now.date() + datetime.timedelta(days=1)).replace(tzinfo=datetime.timezone.utc)
-                    self.logger.debug("Next self.next_sunrise : %s" % self.next_sunrise)
+                    self.logger.debug("Next sunrise : %s" % self.next_sunrise)
                     self.on_self.next_sunset()
 
             else:
@@ -58,16 +57,16 @@ class SunshineTrigger(threading.Thread):
                     self.logger.debug("Day has raised")                    
                     self.sunshine = True
                     self.next_sunset = self.calendar.setutc(now.date()).replace(tzinfo=datetime.timezone.utc)
-                    self.logger.debug("Next self.next_sunset : %s" % self.next_sunset)
+                    self.logger.debug("Next sunset : %s" % self.next_sunset)
                     self.on_self.next_sunrise()
 
             time.sleep(1)
 
     def on_sunrise(self):
-        self.logger.debug("on_self.next_sunrise")
+        self.logger.debug("on_sunrise")
 
     def on_sunset(self):
-        self.logger.debug("on_self.next_sunset")
+        self.logger.debug("on_sunset")
 
     def join(self):
         self.do_run = False
